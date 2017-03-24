c3ebb9fda243d23bf94f524b2765cf583d29dcbc
schoeu <quanyou1013@gmail.com>
2017-03-24 19:05:02 +0800

    update download funcion

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
  * @description Created by schoeu on 2017/3/24.
  */
 
-var request = require('request');
 var fs = require('fs-extra');
+var http = require('http');
+var https = require('https');
 var path = require('path');
+var url = require('url');
 
 var defaultOptions = {
     filesPath: path.join(__dirname, '__tempfile')
@@ -34,17 +36,35 @@ var fild = {
         var me = this;
         this.init(options);
         var srcs = me.srcs;
-
+        var get = http.get;
         for (var i = 0; i < srcs.length; i++) {
             var item = srcs[i];
             if (typeof item === 'string') {
+                var protocol = url.parse(item);
+                var useMethod = /s/.test(protocol);
+
+                if (useMethod) {
+                    get = https.get;
+                }
                 var filename = path.basename(item);
-                request
-                    .get(item)
-                    .on('error', function (err) {
-                        console.log(err);
-                    })
-                    .pipe(fs.createWriteStream(path.join(me.filesPath, filename)));
+                var filePart;
+
+                get(item, function (res) {
+                    var status = res.statusCode;
+                    res.on('data', function (d) {
+                        filePart += d;
+                    });
+                    res.on('end', function () {
+                        fs.writeFile(path.join(me.filesPath, filename), filePart, 'binary', function (err) {
+                            if (err) {
+                                console.log('down fail');
+                            }
+                            console.log('down success');
+                        });
+                    });
+                }).on('error', function (e) {
+                    console.error(e);
+                });;
             }
         }
     }
---
+++ test/api.js
@@ -0,0 +1,9 @@
+/**
+ * Created by schoeu on 2017/3/24.
+ */
+
+var filed = require('../');
+
+filed.download({
+    srcs: ['http://s0.hao123img.com/res/img/logo/logonew.png']
+});
